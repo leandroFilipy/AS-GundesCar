2a70a7ef19c1e8c87c0c1781e2d11123237c61a4
LEANDRO FILIPY DE LIMA <leandro_f_lima@estudante.sesisenai.org.br>
2026-02-12 18:16:36 -0300

    feat: adiciona o prototipo

---
+++ ocp-errado/src/main/java/br/com/senai/centroweg/Main.java
@@ -0,0 +1,102 @@
+package br.com.senai.centroweg;
+
+import br.com.senai.centroweg.model.Carro;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Carro carro = new Carro("GC-1", "Picasso", "Citroen", "Rosa", 2023, 50000.0, 20000);
+
+        double ipva = carro.getPeso()/10;
+        double valorCarro = carro.getPreco();
+        double taxa;
+
+        if(carro.getModelo().equalsIgnoreCase("Picasso")){
+            if(carro.getAno() == 2023){
+                taxa = 1000;
+                valorCarro -= taxa;
+            } else if(carro.getAno() == 2024){
+                taxa = 500;
+                valorCarro -= taxa;
+            } else if(carro.getAno() == 2025){
+                taxa = 100;
+                valorCarro -= taxa;
+            }
+
+        } else if (carro.getModelo().equalsIgnoreCase("Ferrari")) {
+            if(carro.getAno() == 2023){
+                taxa = 1000;
+                valorCarro -= taxa;
+            } else if(carro.getAno() == 2024){
+                taxa = 500;
+                valorCarro -= taxa;
+            } else if(carro.getAno() == 2025){
+                taxa = 100;
+                valorCarro -= taxa;
+            }
+
+        } else if (carro.getModelo().equalsIgnoreCase("Lamborghini")){
+            if(carro.getAno() == 2023){
+                taxa = 1000;
+                valorCarro -= taxa;
+            } else if(carro.getAno() == 2024){
+                taxa = 500;
+                valorCarro -= taxa;
+            } else if(carro.getAno() == 2025){
+                taxa = 100;
+                valorCarro -= taxa;
+            }
+
+        }  else if (carro.getModelo().equalsIgnoreCase("Belina")) {
+            if(carro.getAno() == 2023){
+                taxa = 1000;
+                valorCarro -= taxa;
+            } else if(carro.getAno() == 2024){
+                taxa = 500;
+                valorCarro -= taxa;
+            } else if(carro.getAno() == 2025){
+                taxa = 100;
+                valorCarro -= taxa;
+            }
+
+        } else if (carro.getModelo().equalsIgnoreCase("Aircross")){
+            if(carro.getAno() == 2023){
+                taxa = 1000;
+                valorCarro -= taxa;
+            } else if(carro.getAno() == 2024){
+                taxa = 500;
+                valorCarro -= taxa;
+            } else if(carro.getAno() == 2025){
+                taxa = 100;
+                valorCarro -= taxa;
+            }
+            
+        } else if (carro.getModelo().equalsIgnoreCase("CIVIC")) {
+            if(carro.getAno() == 2023){
+                taxa = 1000;
+                valorCarro -= taxa;
+            } else if(carro.getAno() == 2024){
+                taxa = 500;
+                valorCarro -= taxa;
+            } else if(carro.getAno() == 2025){
+                taxa = 100;
+                valorCarro -= taxa;
+            }
+            
+        } else if (carro.getModelo().equalsIgnoreCase("Kwid")) {
+            if(carro.getAno() == 2023){
+                taxa = 1000;
+                valorCarro -= taxa;
+            } else if(carro.getAno() == 2024){
+                taxa = 500;
+                valorCarro -= taxa;
+            } else if(carro.getAno() == 2025){
+                taxa = 100;
+                valorCarro -= taxa;
+            }
+        }
+
+        System.out.println("O valor do carro é: " + valorCarro);
+    }
+}
